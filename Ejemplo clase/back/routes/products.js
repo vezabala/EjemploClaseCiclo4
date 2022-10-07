caa78f4d576294b1c2fa866f76579cdaa96a5ec6
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-10-07 14:26:11 -0500

    chore: Enrrutador basico

--- Ejemplo clase/back/routes/products.js
+++ Ejemplo clase/back/routes/products.js
@@ -1,1 +1,8 @@
 const express=require("express")
+const router= express.Router();
+
+const{getProducts} = require("../controllers/productsController") //traemos la respuesta json desde el controlador
+
+router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver getproducts
+
+module.exports=router;
