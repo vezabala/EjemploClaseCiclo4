9edfe8e3803dd93b0d26e1cec9275ceb3d010009
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-10-11 02:48:09 -0500

    chore: Conexion a base de datos nodeJs

--- Ejemplo clase/back/server.js
+++ Ejemplo clase/back/server.js
@@ -1,8 +1,13 @@
 const app = require("./app");
+const connectDatabase = require("./config/database");
 
 //SETEAR EL ARCHIVO DE CONFIGURACION
 const dotenv = require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
+
+//Configurar base de datos
+connectDatabase();
+
 
 //llamamos al servidor
 const server=app.listen(process.env.PORT, ()=>{
