889036b33786a20a092c1683f581b2d6b516420c
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-10-17 13:19:40 -0500

    chore: Metodo create productos y entidad productos

--- Ejemplo clase/back/controllers/productsController.js
+++ Ejemplo clase/back/controllers/productsController.js
@@ -1,6 +1,19 @@
+const producto = require("../models/productos")
+
+//Ver lista de productos
 exports.getProducts=(req,res,next) =>{
     res.status(200).json({
         sucess:true,
         message: "En esta ruta usted va a poder ver todos los productos"
+    })
+}
+
+//Crear nuevo producto /api/productos
+exports.newProduct= async(req,res,next) =>{
+    const product= await producto.create(req.body);
+
+    res.status(201).json({
+        sucess:true,
+        product
     })
 }
