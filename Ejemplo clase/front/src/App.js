622b9070b3d5017aa8379741b0cd025e0629d06c
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-10-18 00:15:46 -0500

    chore: configuracion y creacion front en react

---
+++ Ejemplo clase/front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda virtual ejemplo de clase</h1>
+      <img src="./images/logo.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
